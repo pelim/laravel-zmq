73050ae95d61ce0de15536087616ba235ad812a2
Simon Hamp <simon.hamp@me.com>
2017-11-10 15:45:25 +0000

    Fix broadcasting for Laravel 5.5

--- src/Broadcasting/Broadcaster/ZmqBroadcaster.php
+++ src/Broadcasting/Broadcaster/ZmqBroadcaster.php
@@ -2,22 +2,24 @@
 
 namespace Pelim\LaravelZmq\Broadcasting\Broadcaster;
 
-use Illuminate\Contracts\Broadcasting\Broadcaster;
-use Pelim\LaravelZmq\Connector\ZmqConnector;
 use Pelim\LaravelZmq\Zmq;
+use \Illuminate\Http\Request;
+use Pelim\LaravelZmq\Connector\ZmqConnector;
+use Illuminate\Broadcasting\Broadcasters\Broadcaster;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Class ZmqBroadcaster
  * @package Pelim\LaravelZmq\Broadcasting\Broadcaster
  */
-class ZmqBroadcaster implements Broadcaster
+class ZmqBroadcaster extends Broadcaster
 {
 
     /**
      * @var Zmq
      */
     protected $zmq;
-    
+
 
     /**
      * ZmqBroadcaster constructor.
@@ -27,12 +29,47 @@ class ZmqBroadcaster implements Broadcaster
     {
         $this->zmq = $zmq;
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function broadcast(array $channels, $event, array $payload = [])
     {
         $this->zmq->publish($channels, $event, $payload, 'publish');
+    }
+
+    public function auth($request)
+    {
+        if (Str::startsWith($request->channel_name, ['private-', 'presence-']) &&
+            ! $request->user()) {
+            throw new AccessDeniedHttpException;
+        }
+
+        $channelName = Str::startsWith($request->channel_name, 'private-')
+                            ? Str::replaceFirst('private-', '', $request->channel_name)
+                            : Str::replaceFirst('presence-', '', $request->channel_name);
+
+        return parent::verifyUserCanAccessChannel(
+            $request,
+            $channelName
+        );
+    }
+
+    public function validAuthenticationResponse($request, $result)
+    {
+        if (Str::startsWith($request->channel_name, 'private')) {
+            return $this->decodePusherResponse(
+                $this->pusher->socket_auth($request->channel_name, $request->socket_id)
+            );
+        }
+
+        return $this->decodePusherResponse(
+            $this->pusher->presence_auth(
+                $request->channel_name,
+                $request->socket_id,
+                $request->user()->getAuthIdentifier(),
+                $result
+            )
+        );
     }
 }
